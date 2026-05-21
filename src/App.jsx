449d91dc8f428115458f14b1df11ffef7b6f7cd4
richardcreatech <richardcreatech@gmail.com>
2026-05-21 23:39:06 +0100

    Added some dummy data each representing a mix of verified pending and unverified users

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 import logo from "./assets/logo.png";
+import Verify from "./components/Verify";
 
 function App() {
   const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [filled, setFilled] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (name) {
+      setUsername(name);
+      setFilled(!filled);
+    }
+    else {
+      alert("Fill in a Username")
+    }
   };
 
   return (
     <main id="form-page">
+      {username && <Verify username={username} />}
       <form onSubmit={(e) => handleSubmit(e)}>
         <div id="brand">
           <img src={logo} width={40} height={40} />
@@ -16,8 +27,8 @@ function App() {
         </div>
         <br />
         <div id="desc">
-          <h2>Love to Have you {name}</h2>
-          <p>Get verified today </p>
+          <h2>Love to Have you - {name}</h2>
+          <p> Get verified today </p>
         </div>
 
         <div id="fill_me">
@@ -28,7 +39,7 @@ function App() {
             onChange={(e) => setName(e.target.value)}
           />
           <br />
-          <button>Check Me</button>
+          <button type="submit">Check Me</button>
         </div>
       </form>
     </main>
---
+++ src/components/Verify.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import data from "./data.json";
+import logo from "../assets/logo.png";
+import { useState } from "react";
+
+function Verify({ username }) {
+  const my_data = data;
+  const [status, setStatus] = useState("verified");
+  const [categories, setCategories] = useState([]);
+  const [joined, setJoined] = useState("");
+
+  function handle_username() {
+    console.log(typeof username);
+    console.log(my_data[0]);
+    console.log(username);
+
+    let curr_user = my_data.find((i) => i.name == username);
+    setStatus(curr_user.status);
+    setCategories(curr_user.categoried);
+    setJoined(curr_user.joined_date);
+    console.log(curr_user);
+  }
+
+  useEffect(() => {
+    handle_username();
+  }, []);
+
+  return (
+    <section id="verify_me">
+      {/* {username} */}
+      <div id="verification_card">
+        <div id="user_details">
+          <h2>{username}</h2>
+          <span >
+            <strong>{status}</strong>
+                  </span>
+                  {
+                      status == "verified" && (
+                          <ul className="category_list">
+                              {categories.map(i => (
+                                  <li>{i}</li>
+                              ))}
+                          </ul>
+                      )
+
+                      
+                  }
+                  
+                  {status == "pending" && (
+                  <p>I'm Pending</p>
+                  )}
+                  {status == "unverified" && (
+                  <p>I'm Unverified</p>
+                  )}
+
+                  <small>{joined}</small>
+        </div>
+        <img src={logo} alt="" />
+      </div>
+    </section>
+  );
+}
+
+export default Verify;
